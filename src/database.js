fdf6efb1f5915f5246780e869b9640e5d356e570
marianolepera <marianolepera@hotmail.com>
2022-06-17 20:00:08 -0300

    mongo actualizado

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import config from "./config";
 
-mongoose.connect("mongodb+srv://marianolepera:Manko_100@hrensolvers.lkgxrai.mongodb.net/?retryWrites=true&w=majority",{
+mongoose.connect(config.MONGODB_URI_ATLAS,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then((db) => console.log(`base de datos conectada`))
