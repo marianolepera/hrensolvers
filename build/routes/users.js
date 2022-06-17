254418a0ded5ef3934b7cc286413743955ea4b8a
marianolepera <marianolepera@hotmail.com>
2022-06-17 18:48:03 -0300

    build agregada

--- build/routes/users.js
+++ build/routes/users.js
@@ -16,7 +16,7 @@ function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "functio
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 var router = (0, _express.Router)();
-router.get("/", _middlewares.authJwt.verifyToken, usersController.getUsers);
+router.get("/", usersController.getUsers);
 router.get("/:userId", _middlewares.authJwt.verifyToken, usersController.getUserById);
 var _default = router;
 exports.default = _default;
