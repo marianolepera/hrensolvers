583ae83174be00a65d068b04cdc2c89f233a9652
marianolepera <marianolepera@hotmail.com>
2022-06-17 18:30:35 -0300

    backend terminado

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,7 +4,7 @@ const router = Router();
 import * as usersController from "../controllers/usersController";
 import { authJwt } from "../middlewares";
 
-router.get("/",authJwt.verifyToken ,usersController.getUsers)
+router.get("/" ,usersController.getUsers)
 router.get("/:userId",authJwt.verifyToken ,usersController.getUserById)
 
 export default router;
