657bc00526aed76b9f2108b149c8b13c673346e2
marianolepera <marianolepera@hotmail.com>
2022-06-24 05:48:51 -0300

    nube

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import config from "./config";
 
-mongoose.connect(config.MONGODB_URI,{
+mongoose.connect(config.MONGODB_ATLAS,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then((db) => console.log(`base de datos conectada`))
